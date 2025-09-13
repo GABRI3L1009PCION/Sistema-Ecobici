8cda0045a491286c5ddd99c4860c3390c263e8d9
I0NI <irodasc2@miumg.edu.gt>
2025-09-12 23:45:22 -0600

    Creacion de estilos del index

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ try {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap (general) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="cliente/styles/index.css">
   <!-- Font Awesome (iconos) -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 </head>
